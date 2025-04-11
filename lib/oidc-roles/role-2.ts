bfcd384fb4c173dc8fd427a989c96b91dba35ca7
emfrab-qred <emanuel.abitante@qred.com>
2025-04-11 15:57:41 +0200

    Added extra role

--- lib/oidc-roles/role-2.ts
+++ lib/oidc-roles/role-2.ts
@@ -3,25 +3,15 @@ import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { BaseOidcRoleProps, OidcRoleBase } from "./constructs/base";
 
-export class Role1 extends OidcRoleBase {
+export class Role2 extends OidcRoleBase {
   public readonly role: iam.Role;
 
   constructor(scope: Construct, id: string, props: BaseOidcRoleProps) {
     super(scope, id, props);
 
-    this.addAPIGWPermissions();
-
     this.addCustomPermissions({
         actions: [
-          "ecs:CreateTaskSet",
-          "ecs:DeleteTaskSet",
-          "ecs:DescribeServices",
-          "ecs:UpdateService*",
-          "elasticloadbalancing:DescribeListeners",
-          "elasticloadbalancing:DescribeRules",
-          "elasticloadbalancing:DescribeTargetGroups",
-          "elasticloadbalancing:ModifyListener",
-          "elasticloadbalancing:ModifyRule",
+          "ec2:*",
         ],
         resources: ["*"],
         effect: iam.Effect.ALLOW,
--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { MyRole } from './oidc-roles/my-role';
+import { Role1 } from './oidc-roles/role-1';
+import { Role2 } from './oidc-roles/role-2';
 import { BaseOidcRoleProps } from './oidc-roles/constructs/base';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,6 +9,7 @@ export class TestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: BaseOidcRoleProps) {
     super(scope, id, props);
 
-    const myRole = new MyRole(this, "my-custom-role", props);
+    new Role1(this, "my-custom-role-1", props);
+    new Role2(this, "my-custom-role-2", props);
   }
 }
